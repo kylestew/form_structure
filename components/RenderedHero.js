1142147becb38263b81f91d38ffb733002241ec5
Kyle Stewart <kylestew@gmail.com>
2023-12-04 14:10:56 -0700

    Got three.js spinny cube demo in header

--- components/RenderedHero.js
+++ components/RenderedHero.js
@@ -1,29 +1,66 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import * as THREE from "three";
 
 const RenderedHero = () => {
-  const canvasRef = useRef(null);
-
-  const draw = (ctx) => {
-    ctx.fillStyle = "rgba(32, 0, 32, 1.0)"; // Set the color you want to clear with
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Fill the entire canvas
-  };
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
+    const container = containerRef.current;
 
-    // Resize the canvas to fill the container
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+    const canvas = document.createElement("canvas");
+    container.appendChild(canvas);
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-    // Call your draw function
-    draw(context);
-  }, [draw]);
+    // Set up the scene, camera, and renderer
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      container.offsetWidth / container.offsetHeight,
+      0.1,
+      1000
+    );
+
+    // Create a cube
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 2;
+
+    let animationFrameId;
+    const animate = function () {
+      animationFrameId = requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+
+    const handleResize = () => {
+      renderer.setSize(container.clientWidth, container.clientHeight);
+      camera.aspect = container.clientWidth / container.clientHeight;
+      camera.updateProjectionMatrix();
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    animate();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      cancelAnimationFrame(animationFrameId); // Stop the animation
+      renderer.dispose(); // Dispose of the renderer
+
+      if (cube.geometry) cube.geometry.dispose(); // Dispose of the geometry
+      if (cube.material) cube.material.dispose(); // Dispose of the material
+
+      container.removeChild(canvas); // Remove the canvas from the DOM
+    };
+  }, []);
 
   return (
-    <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-full" />
+    <div ref={containerRef} className="absolute top-0 left-0 w-full h-full" />
   );
 };
 
