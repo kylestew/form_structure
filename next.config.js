20a2e24338176e55436b6ccd2192f56eedec2d6b
Kyle Stewart <kylestew@gmail.com>
2023-12-26 13:49:20 -0700

    Got GLSL loading through Webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config, { isServer }) {
+    // Allow importing of shader files (e.g. `.glsl` -- filenames below)
+    // @see: https://github.com/glslify/glslify-loader
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag|ps)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    return config;
+  },
+};
