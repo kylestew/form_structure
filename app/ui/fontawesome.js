10d80346124724db12662f784cd5bd1d899bbb3b
Kyle Stewart <kylestew@gmail.com>
2024-07-21 14:23:21 +0100

    Social icons, yeah!

---
+++ app/ui/fontawesome.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInstagram, faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faInstagram, faTwitter, faGithub, faLinkedin)
