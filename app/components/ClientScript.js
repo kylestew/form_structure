c565f6f6d36593d9e9e2ec62ab80c04564c1322f
Kyle Stewart <kylestew@gmail.com>
2025-03-31 13:34:45 +0100

    Cleanup and simplify notes

--- app/components/ClientScript.js
+++ app/components/ClientScript.js
@@ -5,12 +5,13 @@ import * as geo from 'root/geo'
 import * as array from 'root/array'
 import * as random from 'root/random'
 import * as math from 'root/math'
+import * as matrix from 'gl-matrix'
 
 export default function ClientScript({ code }) {
     useEffect(() => {
         try {
             const fn = new Function('root', code)
-            fn({ geo, array, random, math })
+            fn({ geo, array, random, math, matrix })
         } catch (err) {
             console.error('Error in <ClientScript>', err)
         }
